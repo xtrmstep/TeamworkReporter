70bb8ac4458c7c047df0fbba405ff95015633fcb
Alex Goida <alex.goida@xtrmstep.ru>
2015-02-07 12:39:12 +0300

    first controllers

---
+++ TeamworkReporter/TeamworkReporter/Controllers/ConfigurationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamworkReporter.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
