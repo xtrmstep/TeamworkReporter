2d4c5b1368a0f48704a90cfec1de0da9d5fc3b7a
Alex Goida <alex.goida@xtrmstep.ru>
2015-03-09 11:28:13 +0300

    version is automaticaly generated

--- TeamworkReporter/Types/Extensions/HtmlExtensions.cs
+++ TeamworkReporter/Types/Extensions/HtmlExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Diagnostics;
+using System.Web;
 using System.Web.Mvc;
 
 namespace TeamworkReporter.Types.Extensions
@@ -11,6 +13,29 @@ namespace TeamworkReporter.Types.Extensions
             tb.Attributes["rel"] = "stylesheet";
             tb.Attributes["href"] = url.Content(uri);
             return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+
+        /// <summary>
+        /// Returns a version of the assembly in the starndard format "major-minor-date-build"
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public static IHtmlString GetVersion(this HtmlHelper html)
+        {
+            var assemblyName = typeof (MvcApplication).Assembly.GetName();
+            return new MvcHtmlString(assemblyName.Version.ToString());
+        }
+
+        /// <summary>
+        /// Returns a copyrights
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        public static IHtmlString GetCopyrights(this HtmlHelper html)
+        {
+            var assembly = typeof (MvcApplication).Assembly;
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return new MvcHtmlString(fvi.LegalCopyright);
         }
     }
 }
