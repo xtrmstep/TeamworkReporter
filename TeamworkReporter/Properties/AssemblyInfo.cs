ead0c2de9e8d0500449de7a06d1f92a510349027
Alex Goida <alex.goida@xtrmstep.ru>
2015-04-12 10:32:51 +0300

    Height of the people block in Timelogs is adjusted

--- TeamworkReporter/Properties/AssemblyInfo.cs
+++ TeamworkReporter/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5ba24fa7-ccff-4ec7-9dda-c77fef182a42")]
-[assembly: AssemblyVersion("1.0.37.380")]
-[assembly: AssemblyFileVersion("1.0.37.380")]
+[assembly: AssemblyVersion("1.0.63.846")]
+[assembly: AssemblyFileVersion("1.0.63.846")]
