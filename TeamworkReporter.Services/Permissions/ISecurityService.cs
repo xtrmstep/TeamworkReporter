fde4a63a96d2f89b76bf9bc1b664b060cf22eab5
Alex Goida <alex.goida@xtrmstep.ru>
2015-06-10 11:04:33 +0300

    Renaming to ISecurityService

---
+++ TeamworkReporter.Services/Permissions/ISecurityService.cs
@@ -0,0 +1,26 @@
+﻿using TeamworkReporter.Models;
+using TeamworkReporter.Services.Types;
+
+namespace TeamworkReporter.Services.Permissions
+{
+    /*
+     * What is expected?
+     * helper https://msdn.microsoft.com/en-us/library/webmatrix.webdata.websecurity(v=vs.111).aspx
+     * membership http://www.codeproject.com/Articles/689801/Understanding-and-Using-Simple-Membership-Provider
+     */
+    public interface ISecurityService
+    {
+        Profile Login(string usename, string password);
+        void Logout(Token token);
+        bool RegisterProfile(Profile profile);
+        bool UpdatePassword(Profile profile, string newPassword);
+
+        string ResetPassword(Profile profile);
+
+        /*
+         * RegisterUser
+         * ResetPassword
+         * RenderPassword
+         */
+    }
+}
