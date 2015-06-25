da15cf47db2e501689afc4728bc04bfab294d24f
Alexander Goida <alex.goida@xtrmstep.ru>
2015-06-25 08:20:45 +0300

    Adding missing files

---
+++ TeamworkReporter.Types.Tests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamworkReporter.Types.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamworkReporter.Types.Extensions.Tests
+{
+    [TestClass]
+    public class TypeExtensionsTests
+    {
+        [TestMethod]
+        public void GetKeyProperties_should_return_property_by_attribute()
+        {
+            var pr = typeof(EntityWithKey).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(1, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_return_all_property_by_attribute()
+        {
+            var pr = typeof(EntityWithTwoKeys).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(2, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_return_property_by_match()
+        {
+            var pr = typeof(EntityWithSymbolicKey).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(1, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_return_all_property_by_match()
+        {
+            var pr = typeof(EntityWithTwoSymbolicKey).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(2, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_return_all_property_when_mixed()
+        {
+            var pr = typeof(EntityWithMixedKey).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(2, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_not_return_same_property_twice_by_attr_and_match()
+        {
+            var pr = typeof(EntityWithTwoKeys).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(2, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_return_empty()
+        {
+            var pr = typeof(EntityWithoutKey).GetKeyProperties();
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(0, pr.Length);
+        }
+
+        [TestMethod]
+        public void GetKeyProperties_should_return_null()
+        {
+            Type t = null;
+            var pr = t.GetKeyProperties();
+            Assert.IsNull(pr);
+        }
+        //todo !!! separate test classes from the tests
+        public class EntityWithKey
+        {
+            [Key]
+            public int Id { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        public class EntityWithSymbolicKey
+        {
+            public int Id { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        public class EntityWithTwoKeys
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Key]
+            public int Rank { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        public class EntityWithTwoSymbolicKey
+        {
+            public int Id { get; set; }
+
+            public int RankId { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        public class EntityWithMixedKey
+        {
+            public int Id { get; set; }
+
+            [Key]
+            public int Rank { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        public class EntityWithoutKey
+        {
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ TeamworkReporter.Types/Extensions/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamworkReporter.Types.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static PropertyInfo[] GetKeyProperties(this Type type)
+        {
+            if (type == null) return null;
+
+            var propertiesByAttr = type.GetProperties()
+                .Where(p => p.GetCustomAttribute<KeyAttribute>() != null);
+
+            var propertiesByMatch = type.GetProperties()
+                .Where(p => p.Name.ToUpper().Contains("ID") && propertiesByAttr.Any(pa => pa.Name == p.Name) == false);
+
+            var properties = propertiesByAttr.Concat(propertiesByMatch).ToArray();
+
+            return properties;
+        }
+    }
+}
